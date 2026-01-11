857a0497c766aea1ee4026479fd859a48eb4821c
cornishman2 <cornishman2@gmail.com>
2026-01-11 19:48:25 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dig-it-cache-v7"; // <-- bump this on every deployment
+const CACHE_NAME = "dig-it-cache-v8"; // <-- bump this on every deployment
 
 const APP_SHELL = [
   "./",
