ac30244be89bc427a28d294f38862177a69c02ca
cornishman2 <cornishman2@gmail.com>
2026-01-15 12:04:55 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "detekta-cache-v11"; // <-- bump this on every deployment
+const CACHE_NAME = "detekta-cache-v12"; // <-- bump this on every deployment
 
 const APP_SHELL = [
   "./",
